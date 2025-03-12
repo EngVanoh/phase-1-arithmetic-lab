8986a734c1afef9bb434710db56ec63465264832
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-03-13 00:13:50 +0300

    arithmetic1

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()* 10)
+let num3 = 12
+let num4 = 3
+
+let mod = num3 /num4
+console.log(mod)
+
+let max = Math.max(1, 2, 20, 4, 5);
+console.log(max)
 
 // Write your code here
